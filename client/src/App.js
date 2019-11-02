326fb17657e3a684e5f06f2658a5825d9a7acab4
Hari <augten95@gmail.com>
2019-11-02 18:21:03 -0500

    Imported all components; created, configured, and rendered app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
+import Nav from "./components/Nav";
+import { Container } from "./components/Grid";
+import { ToastContainer, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Router>
+          <Container>
+            <ToastContainer
+              position="top-center"
+              autoClose={5000}
+              transition={Zoom}
+              hideProgressBar
+              newestOnTop
+              closeOnClick
+              rtl={false}
+              pauseOnVisibilityChange
+              draggable={false}
+              pauseOnHover
+              />
+                <Switch>
+                  <Route exact path="/" component={Search} />
+                  <Route path="/bookshelf" component={Saved} />
+                  <Route exact path="/books/:id" component={null} />
+                  <Route component={null} />
+                </Switch>
+          </Container>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
