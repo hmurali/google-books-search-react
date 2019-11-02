a23111bdba5471d47bb3d4b1271b2fe04d01809d
Hari <augten95@gmail.com>
2019-11-02 18:23:24 -0500

    Added build folder and added .cache to client/node_modules

---
+++ client/build/precache-manifest.9790569aef345f5c599a4caef851fe45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfbe3e5173a9a39d3e1b10bbb723036",
+    "url": "/index.html"
+  },
+  {
+    "revision": "590dc3f833eb732116a6",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c9b4e3b5a65aaf717049",
+    "url": "/static/js/2.de72054c.chunk.js"
+  },
+  {
+    "revision": "590dc3f833eb732116a6",
+    "url": "/static/js/main.f58b0c53.chunk.js"
+  },
+  {
+    "revision": "6729cf4c06b6b76ea1d6",
+    "url": "/static/js/runtime-main.57d168b9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
